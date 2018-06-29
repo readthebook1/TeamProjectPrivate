f5501a231ba2444e0dbda4375bdabcdbd709c194
user <user@DESKTOP-1N7TOI2>
2018-06-29 14:01:51 +0900

    Member객체에 Tel 추가 logic의 DI 객체별로 주석 달아놨습니다. 작성하실 때 참고 부탁드리겠습니다.

--- src/main/java/logic/Member.java
+++ src/main/java/logic/Member.java
@@ -3,17 +3,20 @@ package logic;
 import java.util.Date;
 
 public class Member {
-	private String id;
-	private String pw;
-	private String name;
-	private String email;
-	private String mob;
-	private Date regDate;
-	private Integer memType;
-	private String hostName;
-	private Integer hostRegNo;
-	private String address;
-	private String accountNo;
+	private String id;		// ID
+	private String pw;		// Password
+	private String name;	// 이름
+	private String email;	// 이메일
+	private String mob;		// 휴대폰
+	private Date regDate;	// 가입일자 (회원가입시 자동으로 입력되게끔) now()
+	private Integer memType;// 회원 상태 0:게스트, 1:호스트
+	
+	/* 호스트계정으로 전환 시 사용 될 항목들 */
+	private String hostName;	// 호스트계정명( ex:상호명 )
+	private Integer hostRegNo;	// 사업자등록번호
+	private String address;		// 사업자주소지
+	private String accountNo;	// 계좌정보 (은행 + 계좌번호 + 예금주)
+	private String tel;			// 사업자 연락처
 	
 	public String getId() {
 		return id;
@@ -81,11 +84,18 @@ public class Member {
 	public void setAccountNo(String accountNo) {
 		this.accountNo = accountNo;
 	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
 	
 	@Override
 	public String toString() {
 		return "Member [id=" + id + ", pw=" + pw + ", name=" + name + ", email=" + email + ", mob=" + mob + ", regDate="
 				+ regDate + ", memType=" + memType + ", hostName=" + hostName + ", hostRegNo=" + hostRegNo
-				+ ", address=" + address + ", accountNo=" + accountNo + "]";
+				+ ", address=" + address + ", accountNo=" + accountNo + ", tel=" + tel + "]";
 	}
+	
 }
